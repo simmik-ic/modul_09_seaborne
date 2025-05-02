354d4fb7cee9fba7010dd96bb8797cf7280691d5
Szymon Mikulski <simmik@poczta.onet.pl>
2025-05-02 22:57:54 +0200

    Add analysis

--- zaleznosci.py
+++ zaleznosci.py
@@ -18,7 +18,7 @@ df['DOB'] = df['DOB'].apply(lambda d: d if d.year < 2025 else d.replace(year=d.y
 df['DateofTermination'] = pd.to_datetime(df['DateofTermination'],format='%m/%d/%y')
 df['DateofHire'] = pd.to_datetime(df['DateofHire'],format='%m/%d/%Y')
 
-#funkcja do obliczania stażu pracy:
+#funkcja do obliczania stażu pracy
 def count_seniority(row):
     if pd.isnull(row['DateofTermination']):
         end_date = dt.datetime(2019,9,27)
@@ -29,10 +29,10 @@ def count_seniority(row):
 #dołącz kolumnę stażu pracy
 df['Seniority'] = df.apply(lambda row: count_seniority(row),axis=1)
 
-#funkcja do obliczania wieku pracownik:
+#funkcja do obliczania wieku pracownika
 def count_age(row):
     return (dt.datetime(2019,9,27) - row['DOB'])/np.timedelta64(1,'D') / 365.25
-#dołącz kolumnę stażu pracy
+#dołącz kolumnę wieku pracownika
 df['Age'] = df.apply(lambda row: count_age(row),axis=1)
 
 #-----------------------------------------------------------------------------------------------------
@@ -94,5 +94,23 @@ plt.gca().invert_yaxis()
 
 
 #-----------------------------------------------------------------------------------------------------
+analysis = """
+1. 
+Pracownicy podlegający pod niektórych menedżerów (Alex Sweetwater, Brannon Miller, Eric Dougall, Janet Kong, Jennifer Zamora) 
+częściej posiadają ocenę 'Exceeds'. Natomiast ci podlegający pod menedżera Debra Houlihan mają więcej ocen 'Needs Improvement'.
+2.
+Pomijając źródła 'Other', aby zapewnić jak najdłuższe zatrudnienie należy skupić się na źródłach pozyskania pracowników takich jak: 
+'Billboards', 'Website Banner Ads', 'On-campus Recruiting', 'Information Sessions' oraz 'MBTA Ads'.
+3.
+Najmniej zadowoleni są pracowanicy 'Married', zaś najbardziej 'Widowed'.
+4.
+Najwięcej pracowników ma od 30 do 45 lat. W starszych rocznikach liczba zatrudnionych spada wraz z wiekiem, z wyjątkiem grupy 65+.
+W firmie nie pracują osoby w wieku poniżej 27 lat, co wydaje się trochę dziwne. Biorąc po uwagę, że ostatnie daty zatrudnienia
+przypadają na 2017 rok (a zakończenia pracy na 2016), być może aktualność bazy danych jest wcześniejsza niż 27 września 2019.
+5.
+Większą liczbę specjalnych projektów realizują młodzi pracownicy.
+"""
+
+print(analysis)
 plt.show()
 plt.close('all')
